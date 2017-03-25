0a72b17d4d52f17d8f419ef201c41b452bf8a2af
T-102 <T-102@PC184>
2017-03-25 10:41:44 -0600

    MVC creacion del modelo

--- src/java/controller/ServletAutenticar.java
+++ src/java/controller/ServletAutenticar.java
@@ -22,8 +22,7 @@ public class ServletAutenticar extends HttpServlet {
     
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("Application/vnd.ms-excel");
-        PrintWriter salida= response.getWriter();
-        salida.println("Yo soy paquita cabeza");
+        
+       
         }
     }
